9dbe6328db2cf2753e08141a4a86bcdd0ba16d44
Tamara Fedorova <koul.slou@gmail.com>
2015-12-02 14:41:40 -0500

    Lesson 1.6 Higher price on Azurite

--- app.js
+++ app.js
@@ -5,12 +5,20 @@
 
 (function(){
 
-    var gem = { name: 'Azurite', price: 2.95};
+
     var app = angular.module('gemStore', []);
 
     app.controller('StoreController', function(){
         this.product = gem;
     });
+
+
+    var gem = {
+        name: 'Azurite',
+        price: 110.50,
+        canPurchase: false,
+        soldOut: true
+    };
 
 })();
 
