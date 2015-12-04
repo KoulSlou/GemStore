bbe60fe149d801313f6d48160b29fe3e2f3c18d6
Tamara Fedorova <koul.slou@gmail.com>
2015-12-04 13:36:12 -0500

    Lesson 5.2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 (function(){
 
 
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-directives']);
 
     app.controller('StoreController', function(){
         this.products = gems;
@@ -23,22 +23,6 @@
         };
     });
 
-
-
-    app.directive('productGallery',function(){
-        return {
-            restrict: 'E',
-            templateUrl:'product-gallery.html',
-            controller: function(){
-                this.current = 0;
-                this.setCurrent = function(newGallery){
-                    this.current = newGallery || 0;
-                };
-            },
-            controllerAs:'gallery'
-        };
-    });
-
     app.controller('ReviewController', function(){
 
         this.review = {};
@@ -50,38 +34,6 @@
         };
     });
 
-    app.directive("productDescription", function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-description.html"
-        };
-    });
-
-    app.directive("productSpecs", function(){
-        return {
-            restrict: 'A',
-            templateUrl: 'product-specs.html'
-        }
-    });
-
-    app.directive("productTabs", function(){
-        return {
-            restrict: "E",
-            templateUrl: "product-tabs.html",
-            controller:function(){
-                this.tab = 1;
-
-                this.isSet = function(checkTab) {
-                    return this.tab === checkTab;
-                };
-
-                this.setTab = function(setTab) {
-                    this.tab = setTab;
-                };
-            },
-            controllerAs: 'tab'
-        };
-    });
 
     var gems = [{
         name: 'Azurite',
